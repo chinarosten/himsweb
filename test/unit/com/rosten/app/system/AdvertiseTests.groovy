70da2cd21f963ad3ed1c42bf5953b52b410d3b51
chinarosten <luhangyu2000@163.com>
2013-11-16 23:03:07 +0800

    first commit

---
+++ test/unit/com/rosten/app/system/AdvertiseTests.groovy
@@ -0,0 +1,17 @@
+package com.rosten.app.system
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Advertise)
+class AdvertiseTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
