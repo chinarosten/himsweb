53eddeaa5ae75c4c13a8af1b3215edec0137c516
chinarosten <luhangyu2000@163.com>
2013-11-23 10:10:31 +0800

    服务器获取

--- web-app/js/rosten/rosten.js
+++ web-app/js/rosten/rosten.js
@@ -1,25 +1,18 @@
 /**
  * @author rosten
- * @created 2013-11-15
- * 
+ * @created 2013-11-22
+ *
  */
 (function() {
 	rostenhas = {
 		jsweb : "jsweb"
 	};
-	rostenhas.loadBaseJs = function(jsweb) {
-		var href = window.location.href;
-		var isDebug = "false";
-		if (href.indexOf("isDebug") != -1) {
-			isDebug = "true";
-		}
-		var _jsweb;
-		if(jsweb){
-			_jsweb = jsweb;
-		}else{
-			_jsweb = rostenhas.jsweb;
-		}
-		var dojolib = "/" + _jsweb + "/dojo/dojo.js";
-		document.write("<script type=\"text/javascript\" src=\"" + dojolib + "\" data-dojo-config=\"parseOnLoad:true,has:{'dojo-firebug':" + isDebug + "},isDebug:1,async:true\"><\/script>");
+	dojoConfig = {
+		has : {
+			"dojo-firebug" : true,
+			"dojo-debug-messages" : true
+		},
+		parseOnLoad : true,
+		async : true
 	};
 })();
