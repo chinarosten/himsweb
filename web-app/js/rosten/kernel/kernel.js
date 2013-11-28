e8dc3a209347768f78241fae76044ebccd909f71
chinarosten <luhangyu2000@163.com>
2013-11-28 17:31:05 +0800

    2013-11-28

---
+++ web-app/js/rosten/kernel/kernel.js
@@ -0,0 +1,11 @@
+/**
+ * @author rosten
+ * @created 2013-11-27
+ * control version
+ */
+define([
+	"dojo/_base/window",
+	], function(win, kernel, lang, xhr, domConstruct, dom, has, domClass) {
+	
+	return kernel;
+});
