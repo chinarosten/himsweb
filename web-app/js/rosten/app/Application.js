42f92b4da523a6a2c754d94abdc7beee5af1d662
chinarosten <luhangyu2000@163.com>
2013-12-01 23:23:02 +0800

    2013-12-01

---
+++ web-app/js/rosten/app/Application.js
@@ -0,0 +1,31 @@
+/**
+ * @author rosten
+ */
+define(["dojo/_base/window"], function(win) {
+
+    rosten.cssinitcommon = function() {
+        //此功能只添加css文件
+        var _rosten = window.opener.rosten;
+        var dojocss = _rosten.dojothemecss;
+        var rostencss = _rosten.rostenthemecss;
+
+        rosten.addDojoThemeCss(dojocss);
+        rosten.addRostenCss(rostencss);
+    };
+    rosten.cssinit = function() {
+        var _rosten = window.opener.rosten;
+        var dojocss = _rosten.dojothemecss;
+        var rostencss = _rosten.rostenthemecss;
+
+        rosten.replaceDojoTheme(dojocss, false);
+        rosten.replaceRostenTheme(rostencss);
+    };
+    /*
+     * 关闭当前窗口，并刷新父文档视图
+     */
+    rosten.pagequit = function() {
+
+        window.opener.rosten.kernel.refreshGrid();
+        window.close();
+    };
+});
