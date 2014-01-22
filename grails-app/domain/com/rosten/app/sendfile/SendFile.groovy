70ee10018aeb5ba1ae6b59e35d8390ad6e847802
chinarosten <luhangyu2000@163.com>
2014-01-23 00:08:36 +0800

    完善sendfile的domain

--- grails-app/domain/com/rosten/app/sendfile/SendFile.groovy
+++ grails-app/domain/com/rosten/app/sendfile/SendFile.groovy
@@ -13,18 +13,97 @@ class SendFile {
 	
 	String id
 	
-	//标题
-	String subject
+	//发文流水号
+	@GridColumn(name="流水号",width="60px")
+	String serialNo
+	
+	//成文日期
+	@GridColumn(name="成文日期")
+	Date fileDate
+	
+	//文件编号
+	@GridColumn(name="文件编号")
+	String fileNo
+	
+	//文件种类
+	String fileType 
+	
+	//文件题名
+	@GridColumn(name="文件题名")
+	String title
 	
 	//主题词
-	String subjectKey
+	String subject
 	
+	//主送单位
+	String mainSend
+	
+	//抄送单位
+	String copyTo
+	
+	//内部抄送
+	String insideCopy
+	
+	//缓急
+	String emergency
+	
+	//印发纷数
+	Integer printCopy
+	
+	//档案种类
+	String archiveType
+	
+	//归档到
+	String archiveDbName
+	
+	//份数
+	Integer copys
+	
+	//页数
+	Integer pages
+	
+	//密级
+	String secretLevel
+	
+	//期限
+	String term
+	
+	//类目号
+	String archiveCategory
+	
+	//相关号
+	String archiveNo
+	
+	//相关卷
+	String referrenceNo
+	
+	//全宗号
+	String fonds
+	
+	//卷号
+	String volumnNo
+	
+	//件号
+	String unitNo
+	
+	//附件
 	List attachments
 	static hasMany=[attachments:Attachment]
 	
+	//缺省读者，读者
+	List defaultReaders,readers
 	
 	//拟稿人
+	@GridColumn(name="拟稿人",colIdx=7)
 	User drafter
+	
+	//当前处理人
+	@GridColumn(name="处理者",colIdx=6)
+	User currentUser
+	
+	//状态
+	@GridColumn(name="状态",colIdx=5)
+	String status
 	
 	//拟稿时间
 	@GridColumn(name="拟稿时间",width="150px")
@@ -35,12 +114,33 @@ class SendFile {
 		return sd.format(createDate)
 	}
 	
+	static transients = [
+		"formattedDate"
+	]
+	
 	static belongsTo = [company:Company]
 	
     static constraints = {
-		subjectKey nullable:true,blank:true
-		
-		
+		fileDate nullable:true,blank:true
+		fileNo nullable:true,blank:true
+		subject nullable:true,blank:true
+		mainSend nullable:true,blank:true
+		copyTo nullable:true,blank:true
+		insideCopy nullable:true,blank:true
+		emergency nullable:true,blank:true
+		printCopy nullable:true,blank:true
+		archiveType nullable:true,blank:true
+		archiveDbName nullable:true,blank:true
+		copys nullable:true,blank:true
+		pages nullable:true,blank:true
+		secretLevel nullable:true,blank:true
+		term nullable:true,blank:true
+		archiveCategory nullable:true,blank:true
+		archiveNo nullable:true,blank:true
+		referrenceNo nullable:true,blank:true
+		fonds nullable:true,blank:true
+		volumnNo nullable:true,blank:true
+		unitNo nullable:true,blank:true
     }
 	
 	static mapping = {
---
+++ grails-app/domain/com/rosten/app/sendfile/SendLable.groovy
@@ -0,0 +1,56 @@
+package com.rosten.app.sendfile
+
+import com.rosten.app.system.Company
+import com.rosten.app.annotation.GridColumn
+
+//发文代字
+class SendLable {
+	
+	String id
+	
+	//代字类型
+	@GridColumn(name="类型",width="60px")
+	String category
+	
+	//二级单位名称
+	String org2Name
+	
+	//发文代字
+	@GridColumn(name="发文代字",width="60px")
+	String subCategory
+	
+	//今年年代
+	@GridColumn(name="今年",width="60px")
+	Integer nowYear
+	
+	//今年文号
+	@GridColumn(name="最新号")
+	Integer nowSN = 1
+	
+	//今年保留号或者废号
+	List nowCancel
+	
+	//去年年代
+	@GridColumn(name="去年",width="60px")
+	Integer frontYear
+	
+	//去年文号
+	@GridColumn(name="最新号")
+	Integer frontSN = 1
+	
+	//去年保留号或者废号
+	List frontCancel
+	
+	static belongsTo = [company:Company]
+
+    static constraints = {
+		nowCancel nullable:true,blank:true
+		frontCancel nullable:true,blank:true
+		org2Name nullable:true,blank:true
+    }
+	
+	static mapping = {
+		id generator:'uuid.hex',params:[separator:'-']
+		table "ROSTEN_SENDFILE_LABLE"
+	}
+}
