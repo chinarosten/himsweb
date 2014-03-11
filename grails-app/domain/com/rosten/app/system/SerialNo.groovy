31cf88da59867fbfb9778065fbc25e4a9a3c95a3
chinarosten <luhangyu2000@163.com>
2014-03-11 17:33:19 +0800

    增加序列号domain

---
+++ grails-app/domain/com/rosten/app/system/SerialNo.groovy
@@ -0,0 +1,16 @@
+package com.rosten.app.system
+
+class SerialNo {
+	String id
+	
+	String macAddress
+	
+	String serialNo
+	
+    static constraints = {
+    }
+	static mapping = {
+		id generator:'uuid.hex',params:[separator:'-']
+		table "ROSTEN_SERIALNO"
+	}
+}
