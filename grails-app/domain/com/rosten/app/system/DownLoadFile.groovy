7c1a8135d1f516de8440be755d5e6cf2738b0b45
chinarosten <luhangyu2000@163.com>
2014-04-16 06:59:53 +0800

    增加文件下载domain

---
+++ grails-app/domain/com/rosten/app/system/DownLoadFile.groovy
@@ -0,0 +1,46 @@
+package com.rosten.app.system
+
+import com.rosten.app.annotation.GridColumn
+import java.text.SimpleDateFormat
+
+class DownLoadFile {
+	String id
+	
+	//标题
+	String subject
+	
+	//发布人
+	User publisher
+	
+	//发布时间
+	Date publishDate = new Date()
+	
+	@GridColumn(name="拟稿时间",width="106px")
+	def getFormattedPublishDate(){
+		if(publishDate!=null){
+			SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd HH:mm")
+			return sd.format(publishDate)
+		}else{
+			return ""
+		}
+	}
+	
+	//描述
+	String description
+	
+	//附件
+	Attachment attachment
+	
+	static belongsTo = [company:Company]
+	
+    static constraints = {
+		attachment nullable:true,blank:true
+		description nullable:true,blank:true
+    }
+	
+	static mapping = {
+		id generator:'uuid.hex',params:[separator:'-']
+		table "ROSTEN_DOWNLOADFILE"
+		description sqlType:"longtext"
+	}
+}
