beb6984ad49029a267d9fe4c8164479577395439
chinarosten <luhangyu2000@163.com>
2014-04-20 10:29:40 +0800

    暂时去除ipad的移动设备判断

--- grails-app/controllers/com/rosten/app/system/LoginController.groovy
+++ grails-app/controllers/com/rosten/app/system/LoginController.groovy
@@ -215,7 +215,7 @@ class LoginController {
 	//判断是否手机客户端
 	def boolean JudgeIsMobile(String requestHeader) {
 		boolean isMoblie = false;
-		def mobileAgents = [ "iphone", "android", "phone", "mobile", "wap", "netfront", "java", "opera mobi",
+		def mobileAgents = [ "iphone", "android", "phone",  "wap", "netfront", "java", "opera mobi",
 				"opera mini", "ucweb", "windows ce", "symbian", "series", "webos", "sony", "blackberry", "dopod",
 				"nokia", "samsung", "palmsource", "xda", "pieplus", "meizu", "midp", "cldc", "motorola", "foma",
 				"docomo", "up.browser", "up.link", "blazer", "helio", "hosin", "huawei", "novarra", "coolpad", "webos",
@@ -224,7 +224,7 @@ class LoginController {
 				"pantech", "gionee", "portalmmm", "jig browser", "hiptop", "benq", "haier", "^lct", "320x320",
 				"240x320", "176x220", "w3c ", "acs-", "alav", "alca", "amoi", "audi", "avan", "benq", "bird", "blac",
 				"blaz", "brew", "cell", "cldc", "cmd-", "dang", "doco", "eric", "hipt", "inno", "ipaq", "java", "jigs",
-				"kddi", "keji", "leno", "lg-c", "lg-d", "lg-g", "lge-", "maui", "maxo", "midp", "mits", "mmef", "mobi",
+				"kddi", "keji", "leno", "lg-c", "lg-d", "lg-g", "lge-", "maui", "maxo", "midp", "mits", "mmef",
 				"mot-", "moto", "mwbp", "nec-", "newt", "noki", "oper", "palm", "pana", "pant", "phil", "play", "port",
 				"prox", "qwap", "sage", "sams", "sany", "sch-", "sec-", "send", "seri", "sgh-", "shar", "sie-", "siem",
 				"smal", "smar", "sony", "sph-", "symb", "t-mo", "teli", "tim-", "tosh", "tsm-", "upg1", "upsi", "vk-v",
@@ -233,6 +233,7 @@ class LoginController {
 		if (requestHeader != null) {
 			for (String mobileAgent : mobileAgents) {
 				if (requestHeader.toLowerCase().indexOf(mobileAgent) >= 0) {
+					println mobileAgent
 					isMoblie = true;
 					break;
 				}
