11ddfb16f5cd90710b57d0eeb711e6aff245c089
chinarosten <luhangyu2000@163.com>
2014-09-04 00:28:34 +0800

    2014-9-4

--- web-app/js/rosten/widget/RostenContentPane.js
+++ web-app/js/rosten/widget/RostenContentPane.js
@@ -1,12 +1,8 @@
-/**
- * @author rosten
- */
 define(["dojo/_base/declare",
-		"dojo/_base/connect",
 		"dijit/_WidgetBase", 
 		"dijit/_TemplatedMixin",
 		"dojox/layout/ContentPane",
-		], function(declare,connect,_WidgetBase,_TemplatedMixin,ContentPane) {
+		], function(declare,_WidgetBase,_TemplatedMixin,ContentPane) {
 	return declare("rosten.widget.RostenContentPane", [_WidgetBase, _TemplatedMixin], {
 		templateString: '<div data-dojo-attach-point="containerNode"></div>',
         contentPane: null,
@@ -17,7 +13,7 @@ define(["dojo/_base/declare",
                 executeScripts:true,
                 style:{"padding":"2px","paddingLeft":"0px","paddingRight":"0px"}
             });
-            this.contentPane.setHref(this.src);
+            this.contentPane.set("href",this.src);
             this.containerNode.appendChild(this.contentPane.domNode);
         },
         destroy: function(){
